63000e84b5ee7a7ae38c6e6a78c1d1e6d1a59b95
Ivenk <ivenkoethke@hotmail.de>
2021-06-23 11:13:30 +0200

    Removed unused dependencies

--- src/main/kotlin/com/koethke/bubbles/BubblesApplication.kt
+++ src/main/kotlin/com/koethke/bubbles/BubblesApplication.kt
@@ -2,9 +2,6 @@ package com.koethke.bubbles
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class BubblesApplication
