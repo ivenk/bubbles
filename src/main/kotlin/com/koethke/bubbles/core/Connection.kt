13b8673cf2c1f1c23ce6d85fcf70b7c446f42710
Ivenk <ivenkoethke@hotmail.de>
2021-06-23 14:26:44 +0200

    Corrected mistake in connection constructor parameters

--- src/main/kotlin/com/koethke/bubbles/core/Connection.kt
+++ src/main/kotlin/com/koethke/bubbles/core/Connection.kt
@@ -1,3 +1,3 @@
 package com.koethke.bubbles.core
 
-data class Connection(val firstID: Unit, val secondID : Unit, val valueMap : Map<String, String>)
+data class Connection(val firstID: String, val secondID : String, val valueMap : Map<String, String>)
